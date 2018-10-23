1e1c0def31fb05b81a1978a66c9285903f167c8d
Brayan Lemus <brayan@nclouds.com>
2018-10-23 17:13:03 -0600

    Mongo update

---
+++ mongodb3/recipes/shutdown.rb
@@ -0,0 +1,49 @@
+include_recipe "mongodb3::mongo_gem"
+
+require 'json'
+require 'mongo'
+require 'bson'
+require 'aws-sdk-opsworks'
+require 'aws-sdk-route53'
+
+# Obtaning mongo instnaces
+this_instance = search("aws_opsworks_instance", "self:true").first
+layer_id = this_instance["layer_ids"][0]
+mongo = Mongo::Client.new([ '127.0.0.1' ], :database => "admin", :connect => "direct", :server_selection_timeout => 5)
+opsworks = Aws::OpsWorks::Client.new(:region => "us-east-1")
+dns = Aws::Route53::Client.new(:region => "#{node['Region']}")
+
+ruby_block 'Removing DNS record' do
+  block do
+    dnsrsets = dns.list_resource_record_sets({
+      hosted_zone_id: "#{node['HostedZoneId']}",
+    })
+
+    dnsrsets.resource_record_sets.each do |old_record|
+      if "#{old_record.resource_records[0].value}" == "#{this_instance["private_ip"]}"
+        Chef::Log.info "Removing RecordSet: " + old_record.name.to_s
+        resp = dns.change_resource_record_sets({
+          change_batch: {
+            changes: [
+              {
+                action: "DELETE",
+                resource_record_set: {
+                  name: "#{old_record.name}",
+                  resource_records: [
+                    {
+                      value: "#{old_record.resource_records[0].value}",
+                    },
+                  ],
+                  ttl: 60,
+                  type: "A",
+                },
+              },
+            ],
+            comment: "Mongo service discovery for #{node['HostID']}",
+          },
+          hosted_zone_id: "#{node['HostedZoneId']}",
+        })
+      end
+    end
+  end
+end
